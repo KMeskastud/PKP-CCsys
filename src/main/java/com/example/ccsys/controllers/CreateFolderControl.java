2ea8d7e35536132662868d0b86b0cb0d4141f96b
RobertasPaskevic <rainis1999@gmail.com>
2022-04-04 23:02:06 +0300

    FO1 feature input validation
    FO1 funkcijai sukurta ivesties validacija

--- src/main/java/com/example/ccsys/controllers/CreateFolderControl.java
+++ src/main/java/com/example/ccsys/controllers/CreateFolderControl.java
@@ -58,6 +58,12 @@ public class CreateFolderControl {
         this.goBack();
     }
 
+  private boolean isValidInput(String input) {
+        if (input.length() == 0)
+            return false;
+        return true;
+    }
+
     public void goBack() throws IOException, SQLException {
         if(loggedInUser.getPosition().equals("Super")) {
             FXMLLoader fxmlLoader = new FXMLLoader(Start.class.getResource("main-window-admin.fxml"));
